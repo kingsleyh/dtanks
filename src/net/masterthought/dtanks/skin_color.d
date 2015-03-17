a3be6d822ba955e6851b49ca3fc8a03372973f2f
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2015-03-17 18:37:56 +0000

    added skin color

---
+++ src/net/masterthought/dtanks/skin_color.d
@@ -0,0 +1,14 @@
+module net.masterthought.dtanks.skincolor;
+
+enum SkinColor{
+  black = "black",
+  blue = "blue",
+  green = "green",
+  orange = "orange",
+  pink = "pink",
+  purple = "purple",
+  purple2 = "purple2",
+  red = "red",
+  white = "white",
+  yellow = "yellow"
+}
